8248cf5bec0da54d3717163e6ea0386185d36a61
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-13 16:48:28 +0900

    [FEAT] MentoryiOS, Onboarding 클래스 생성 & swiftui-combine-mvvm 안에 객체 템플릿 관련 내용 추가

---
+++ Mentory/Mentory/Domain/MentoryiOS/MentoryiOS.swift
@@ -0,0 +1,24 @@
+//
+//  MentoryiOS.swift
+//  Mentory
+//
+//  Created by 김민우 on 11/13/25.
+//
+import Foundation
+import Combine
+
+
+// MARK: Object
+@MainActor
+final class MentoryiOS: Sendable, ObservableObject {
+    // MARK: core
+    
+    
+    // MARK: state
+    
+    
+    // MARK: action
+    
+    
+    // MARK: value
+}
---
+++ Mentory/Mentory/Domain/MentoryiOS/Onboarding/Onboarding.swift
@@ -0,0 +1,24 @@
+//
+//  Onboarding.swift
+//  Mentory
+//
+//  Created by 김민우 on 11/13/25.
+//
+import Foundation
+import Combine
+
+
+// MARK: Object
+@MainActor
+final class Onboarding: Sendable, ObservableObject {
+    // MARK: core
+    
+    
+    // MARK: state
+    
+    
+    // MARK: action
+    
+    
+    // MARK: value
+}
