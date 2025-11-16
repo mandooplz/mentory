b347234bb38fa0264cfc432394fc88e6426f25f3
jaypark <dearjaypark@gmail.com>
2025-11-17 03:07:50 +0900

    [FEAT] MindAnalyzer 생성

---
+++ Mentory/Mentory/Domain/MentoryiOS/MindAnalyzer/CharacterType.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterType.swift
+//  Mentory
+//
+//  Created by JAY on 11/17/25.
+//
+
+import Foundation
+
+// MARK: Object
+enum CharacterType: Sendable {
+    case A
+    case B
+}
---
+++ Mentory/Mentory/Domain/MentoryiOS/MindAnalyzer/MindType.swift
@@ -0,0 +1,19 @@
+//
+//  MindType.swift
+//  Mentory
+//
+//  Created by JAY on 11/17/25.
+//
+
+import Foundation
+
+// MARK: Object
+enum MindType: Sendable {
+    case veryUnpleasant
+    case unPleasant
+    case slightlyUnpleasant
+    case neutral
+    case slightlyPleasant
+    case pleasant
+    case veryPleasant
+}
