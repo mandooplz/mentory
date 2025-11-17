93031569b97891bd375ab9b57243f8f2eee9e2ff
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-18 03:15:41 +0900

    [REFACTOR] MentoryDB.updateName(_:), getName() 구현

--- Mentory/Mentory/Domain/MentoryDB/MentoryDB.swift
+++ Mentory/Mentory/Domain/MentoryDB/MentoryDB.swift
@@ -4,4 +4,24 @@
 //
 //  Created by 김민우 on 11/14/25.
 //
+import Foundation
 
+
+// MARK: Domain
+nonisolated
+struct MentoryDB {
+    // MARK: core
+    nonisolated let id: UUID
+    
+    
+    // MARK: flow
+    @concurrent
+    func updateName(_ newName: String) async throws -> Void {
+        fatalError()
+    }
+    
+    @concurrent
+    func getName() async throws -> String {
+        fatalError()
+    }
+}
