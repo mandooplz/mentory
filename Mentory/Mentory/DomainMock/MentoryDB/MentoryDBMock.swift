0692af093fd2d34f828d2f7f97b9a5dbe5985b83
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-18 03:06:06 +0900

    [REFACTOR] DomainMock 정의

---
+++ Mentory/Mentory/DomainMock/MentoryDB/MentoryDBMock.swift
@@ -0,0 +1,11 @@
+//
+//  MentoryDBMock.swift
+//  Mentory
+//
+//  Created by 김민우 on 11/18/25.
+//
+import Foundation
+
+
+// MARK: Mock
+
---
+++ Mentory/Mentory/DomainMock/MentoryLLM/MentoryLLMMock.swift
@@ -0,0 +1,10 @@
+//
+//  MentoryLLMMock.swift
+//  Mentory
+//
+//  Created by 김민우 on 11/18/25.
+//
+import Foundation
+
+
+// MARK: Mock
