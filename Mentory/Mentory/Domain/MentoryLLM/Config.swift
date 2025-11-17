07cbe649ddb7e2a7f65bb6ecc7358e9dce19fe57
구현모 <funrace252@gmail.com>
2025-11-17 17:21:33 +0900

    [FIX] Config.swift에서 사용자 이름 관련 코드 제거

--- Mentory/Mentory/Domain/MentoryLLM/Config.swift
+++ Mentory/Mentory/Domain/MentoryLLM/Config.swift
@@ -19,14 +19,4 @@ enum Config {
         }
         return token
     }
-
-    /// 사용자 이름 (Secrets.xcconfig의 NAME 값)
-    /// Info.plist에 USER_NAME = $(NAME) 형태로 등록되어야 합니다.
-    static var userName: String {
-        guard let name = Bundle.main.object(forInfoDictionaryKey: "USER_NAME") as? String,
-              !name.isEmpty else {
-            fatalError("USER_NAME이 Info.plist에 설정되지 않았습니다. Secrets.xcconfig의 NAME 값을 Info.plist에 추가해주세요.")
-        }
-        return name
-    }
 }
