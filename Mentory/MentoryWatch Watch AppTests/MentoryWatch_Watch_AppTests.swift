ad3cb7e6e9c343b4be2037684fb4a67b8f2fbcd9
Jiseok Song <jesong1004@naver.com>
2025-11-19 14:08:26 +0900

    [FEAT] 가로형 일기 위젯 및 기록 탭 이동추가

---
+++ Mentory/MentoryWatch Watch AppTests/MentoryWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MentoryWatch_Watch_AppTests.swift
+//  MentoryWatch Watch AppTests
+//
+//  Created by SJS on 11/19/25.
+//
+
+import Testing
+@testable import MentoryWatch_Watch_App
+
+struct MentoryWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
