dcd51c3fbe7862b4ade0c18359c8a2ea6c447761
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-20 11:22:32 +0900

    [REFACTOR] RecordingSheet, WaveFormView 추출

--- Mentory/Mentory/Presentation/Microphone/Microphone.swift
+++ Mentory/Mentory/Presentation/Microphone/Microphone.swift
@@ -5,10 +5,11 @@
 //  Created by 김민우 on 11/20/25.
 //
 import Foundation
+import OSLog
 
 
 // MARK: Object
-@MainActor
+@MainActor @Observable
 final class Microphone: Sendable {
     // MARK: core
     static let shared = Microphone()
@@ -16,10 +17,23 @@ final class Microphone: Sendable {
     
     
     // MARK: state
+    private nonisolated let logger = Logger(subsystem: "MentoryiOS.Microphone", category: "Presentation")
+    var isSetUp: Bool = false
     
+    var isRecording: Bool = false
+    var audioURL: URL? = nil
+    var recordingTime: TimeInterval = 0
     
     
     // MARK: action
+    func setUp() {
+        // capture
+        
+    }
+    
+    func record() {
+        
+    }
     
     
     // MARK: value
