b221b0c4f6ef376828ea54b0c5b813d7d15d3ef2
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-23 01:09:56 +0900

    [TEST] DailyRecordMock, DailyRecordModel 파일 생성

---
+++ Mentory/Mentory/Adapter/MentoryDBMock/DailyRecord/DailyRecordMock.swift
@@ -0,0 +1,7 @@
+//
+//  DailyRecordMock.swift
+//  Mentory
+//
+//  Created by 김민우 on 11/23/25.
+//
+
---
+++ Mentory/Mentory/Adapter/MentoryDBMock/DailyRecord/DailyRecordModel.swift
@@ -0,0 +1,7 @@
+//
+//  DailyRecordModel.swift
+//  Mentory
+//
+//  Created by 김민우 on 11/23/25.
+//
+
