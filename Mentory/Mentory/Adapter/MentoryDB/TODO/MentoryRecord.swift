760970a667db9e66552aaebd47e51d225c43cd8e
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-23 01:08:34 +0900

    [REFACTOR] MentoryRecord, MentoryRecordRepository 주석 처리

--- Mentory/Mentory/Adapter/MentoryDB/TODO/MentoryRecord.swift
+++ Mentory/Mentory/Adapter/MentoryDB/TODO/MentoryRecord.swift
@@ -9,33 +9,33 @@ import SwiftData
 import Foundation
 
 
-// MARK: Model
-@Model
-final class MentoryRecord {
-    // MARK: 기본 정보
-    var id: UUID
-    var recordDate: Date
-
-    // MARK: AI 분석 결과
-    var analyzedContent: String?
-    var emotionType: String?
-
-    // MARK: 메타 데이터
-    var completionTimeInSeconds: TimeInterval?
-
-
-    // MARK: Initializer
-    init(
-        id: UUID = UUID(),
-        recordDate: Date = Date(),
-        analyzedContent: String? = nil,
-        emotionType: String? = nil,
-        completionTimeInSeconds: TimeInterval? = nil
-    ) {
-        self.id = id
-        self.recordDate = recordDate
-        self.analyzedContent = analyzedContent
-        self.emotionType = emotionType
-        self.completionTimeInSeconds = completionTimeInSeconds
-    }
-}
+//// MARK: Model
+//@Model
+//final class MentoryRecord {
+//    // MARK: 기본 정보
+//    var id: UUID
+//    var recordDate: Date
+//
+//    // MARK: AI 분석 결과
+//    var analyzedContent: String?
+//    var emotionType: String?
+//
+//    // MARK: 메타 데이터
+//    var completionTimeInSeconds: TimeInterval?
+//
+//
+//    // MARK: Initializer
+//    init(
+//        id: UUID = UUID(),
+//        recordDate: Date = Date(),
+//        analyzedContent: String? = nil,
+//        emotionType: String? = nil,
+//        completionTimeInSeconds: TimeInterval? = nil
+//    ) {
+//        self.id = id
+//        self.recordDate = recordDate
+//        self.analyzedContent = analyzedContent
+//        self.emotionType = emotionType
+//        self.completionTimeInSeconds = completionTimeInSeconds
+//    }
+//}
