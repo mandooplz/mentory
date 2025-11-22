cb9dc1fa811951a247ff010dfca44789e8ecb3b9
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-23 03:38:00 +0900

    [REFACTOR] MentoryDBMock & MentoryDBModel 구현

--- Mentory/Mentory/Adapter/MentoryDBMock/DailyRecord/DailyRecordMock.swift
+++ Mentory/Mentory/Adapter/MentoryDBMock/DailyRecord/DailyRecordMock.swift
@@ -4,4 +4,18 @@
 //
 //  Created by 김민우 on 11/23/25.
 //
+import Foundation
+import Values
 
+
+// MARK: Mock
+nonisolated
+struct DailyRecordMock: DailyRecordInterface {
+    // MARK: core
+    
+    
+    // MARK: flow
+    func delete() async throws {
+        fatalError()
+    }
+}
--- Mentory/Mentory/Adapter/MentoryDBMock/MentoryDBModel.swift
+++ Mentory/Mentory/Adapter/MentoryDBMock/MentoryDBModel.swift
@@ -5,6 +5,8 @@
 //  Created by 김민우 on 11/18/25.
 //
 import Foundation
+import Collections
+import Values
 
 
 // MARK: Object Model
@@ -16,9 +18,66 @@ final class MentoryDBModel: Sendable {
     // MARK: state
     var userName: String? = nil
     
+    var createRecordQueue: Deque<RecordData> = []
+    
+    var records: [DailyRecordModel] = []
+    func getAllRecords() -> [RecordData] {
+        self.records
+            .map {
+                RecordData(id: $0.id,
+                           createdAt: $0.createAt,
+                           content: $0.content,
+                           analyzedResult: $0.analyzedContent,
+                           emotion: $0.emotion)
+            }
+    }
+    func getTodayRecords() -> [RecordData] {
+        let calendar = Calendar.current
+        
+        return self.records
+            .filter { calendar.isDateInToday($0.createAt) }
+            .map {
+                RecordData(id: $0.id,
+                           createdAt: $0.createAt,
+                           content: $0.content,
+                           analyzedResult: $0.analyzedContent,
+                           emotion: $0.emotion)
+            }
+    }
+    func getRecords(from: Date, to: Date) -> [RecordData] {
+        let start = min(from, to)
+        let end = max(from, to)
+        
+        return self.records
+            .filter { $0.createAt >= start && $0.createAt <= end }
+            .map {
+                RecordData(id: $0.id,
+                           createdAt: $0.createAt,
+                           content: $0.content,
+                           analyzedResult: $0.analyzedContent,
+                           emotion: $0.emotion)
+            }
+    }
+    
     
     // MARK: action
+    func createDailyRecords() {
+        // mutate
+        while createRecordQueue.isEmpty == false {
+            let data = createRecordQueue.removeFirst()
+            
+            let newRecord = DailyRecordModel(
+                owner: self,
+                createAt: data.createdAt,
+                content: data.content,
+                analyzedContent: data.analyzedResult,
+                emotion: data.emotion
+            )
+            
+            records.append(newRecord)
+        }
+    }
     
     
-    // MARK:
+    // MARK: value
 }
