333f2693455ca2578a53172803c33c270f3fb4b4
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-25 03:11:31 +0900

    [TEST] MindAnalyzerTests 테스트 스위트 생성

---
+++ Mentory/MentoryTests/TodayBoard/MindAnalyzerTests.swift
@@ -0,0 +1,47 @@
+//
+//  MindAnalyzerTests.swift
+//  Mentory
+//
+//  Created by 김민우 on 11/25/25.
+//
+import Foundation
+import Testing
+@testable import Mentory
+
+
+// MARK: Tests
+@Suite("MindAnalyzer")
+struct MindAnalyzerTests {
+    
+}
+
+
+// MARK: Helpehr
+private func getRecordFormForTest(_ mentoryiOS: MentoryiOS) async throws -> MindAnalyzer {
+    // MentoryiOS
+    await mentoryiOS.setUp()
+    
+    // Onboarding
+    let onboarding = try #require(await mentoryiOS.onboarding)
+    await onboarding.setName("테스트유저")
+    await onboarding.validateInput()
+    await onboarding.next()
+    
+    // TodayBoard
+    let todayBoard = try #require(await mentoryiOS.todayBoard)
+    await todayBoard.setUpForm()
+    
+    // RecordForm
+    let recordForm = try #require(await todayBoard.recordForm)
+    
+    await MainActor.run {
+        recordForm.titleInput = "SAMPLE_TITLE"
+        recordForm.textInput = "SAMPLE_TEXT"
+    }
+    
+    await recordForm.submit()
+    
+    // MindAnalyzer
+    let mindAnalyzer = try #require(await recordForm.mindAnalyzer)
+    return mindAnalyzer
+}
