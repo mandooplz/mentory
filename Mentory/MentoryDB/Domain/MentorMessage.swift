633f53b8ef017089747645cf85c6dbf6ba0c4141
jaypark <dearjaypark@gmail.com>
2025-11-26 16:03:09 +0900

    [FEAT] MentorMessage 저장을 위한 객체 생성

--- Mentory/MentoryDB/Domain/MentorMessage.swift
+++ Mentory/MentoryDB/Domain/MentorMessage.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftData
 import OSLog
+import Values
 
 // MARK: Object
 actor MentorMessage: Sendable {
@@ -24,6 +25,31 @@ actor MentorMessage: Sendable {
     
     //MARK: value
     //>>여기에 모델.. 어떤 규격?으로 저장될지 정하기 (UUID, 캐릭터타입, 메세지..?)
+    @Model
+    final class MentorMessageModel {
+        // MARK: core
+        @Attribute(.unique) var id: UUID
+        var createdAt: Date
+        var message: String
+        var characterType: CharacterType
+
+        init(id: UUID = UUID(), createdAt: Date, message: String, characterType: CharacterType) {
+            self.id = id
+            self.createdAt = createdAt
+            self.message = message
+            self.characterType = characterType
+        }
+        
+        
+        // MARK: operator
+        func toMessageData() -> MessageData {
+            return .init(id: self.id,
+                         createdAt: self.createdAt,
+                         message: self.message,
+                         characterType: self.characterType)
+        }
+
+    }
     
     
 }
