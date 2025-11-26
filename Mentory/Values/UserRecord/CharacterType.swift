50a24657de600c60ef073e0b3c9d6743e769dc10
jaypark <dearjaypark@gmail.com>
2025-11-26 16:03:09 +0900

    [FEAT] MentorMessage 저장을 위한 객체 생성

---
+++ Mentory/Values/UserRecord/CharacterType.swift
@@ -0,0 +1,30 @@
+//
+//  CharacterType.swift
+//  Values
+//
+//  Created by JAY on 11/26/25.
+//
+
+import Foundation
+
+// MARK: Value
+@frozen
+nonisolated public enum CharacterType: String, Codable, Sendable {
+    case Nangcheol
+    case Gureum
+    
+    var title: String {
+        switch self {
+        case .Nangcheol: return "냉철이가 전하는 한마디"
+        case .Gureum: return "구름이가 전하는 한마디"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .Nangcheol: return "bunsuk"
+        case .Gureum: return "gureum"
+        }
+    }
+}
+
