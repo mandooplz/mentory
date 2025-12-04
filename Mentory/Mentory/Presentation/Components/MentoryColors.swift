a71a97c54389d0849ae28d3bea552a9c11a9cf63
jaypark <dearjaypark@gmail.com>
2025-12-04 11:07:34 +0900

    [FEAT] MentoryColor파일 추가

---
+++ Mentory/Mentory/Presentation/Components/MentoryColors.swift
@@ -0,0 +1,20 @@
+//
+//  MentoryColors.swift
+//  Mentory
+//
+//  Created by JAY on 12/03/25.
+//
+
+import SwiftUI
+
+
+// MARK: Palette
+enum MentoryColor {
+    static let accentPrimary = Color("MentoryAccentPrimary")
+    static let accentSecondary = Color("MentoryAccentSecondary")
+    static let background = Color("MentoryBackground")
+    static let border = Color("MentoryBorder")
+    static let card = Color("MentoryCard")
+    static let progressTrack = Color("MentoryProgressTrack")
+    static let subCard = Color("MentorySubCard")
+}
