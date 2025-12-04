96de84d13cf4e6ab2eb987b871ad5facf6c33bc4
kmw4561@gmail.com <kmw4561@gmail.com>
2025-12-04 09:29:45 +0900

    [REFACTOR] SuggestionView 추가

---
+++ Mentory/Mentory/Presentation/TodayBoard/TodayBoardView/SuggestionView/SuggestionView.swift
@@ -0,0 +1,31 @@
+//
+//  SuggestionView.swift
+//  Mentory
+//
+//  Created by 김민우 on 12/4/25.
+//
+import Foundation
+import SwiftUI
+
+
+
+// MARK: View
+struct SuggestionView: View {
+    var body: some View {
+         Text("Hello, World!")
+    }
+}
+
+
+// MARK: Preview
+fileprivate struct SuggestionPreview: View {
+    var body: some View {
+        Text("SuggestionPreview 프리뷰")
+    }
+}
+
+#Preview {
+    SuggestionPreview()
+}
+
+
