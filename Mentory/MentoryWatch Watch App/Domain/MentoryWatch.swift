1d830f1b576c8da1d10085893a9cbae9df5c3775
kmw4561@gmail.com <kmw4561@gmail.com>
2025-12-10 16:17:06 +0900

    [REFACTOR] MentoryWatch.setUp() 구현

--- Mentory/MentoryWatch Watch App/Domain/MentoryWatch.swift
+++ Mentory/MentoryWatch Watch App/Domain/MentoryWatch.swift
@@ -5,15 +5,19 @@
 //  Created by 김민우 on 11/22/25.
 //
 import Foundation
+import OSLog
 
 
 // MARK: Object
 @MainActor @Observable
 public final class MentoryWatch: Sendable {
     // MARK: core
+    nonisolated let logger = Logger(subsystem: "MentoryWatch", category: "Domain")
+    
     
     
     // MARK: state
+    var isSetUp: Bool = false
     var todayBoard: TodayBoard? = nil
     var actionBoard: ActionBoard? = nil
     
@@ -21,11 +25,15 @@ public final class MentoryWatch: Sendable {
     // MARK: action
     public func setUp() async {
         // capture
+        guard self.isSetUp == false else {
+            logger.error("이미 세팅되어 있습니다.")
+            return
+        }
         
-        // process
         
         // mutate
         self.todayBoard = TodayBoard(owner: self)
         self.actionBoard = ActionBoard(owner: self)
+        self.isSetUp = true
     }
 }
