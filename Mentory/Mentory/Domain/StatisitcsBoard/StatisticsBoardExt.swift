7b15f8049c2ec4e1cf44f551e4c25f5b95c8e336
mandooplz <kmw4561@gmail.com>
2026-02-25 01:46:40 +0900

    MEN-19: 리팩토링을 위한 StatisticsBoardExt 클래스 추가

---
+++ Mentory/Mentory/Domain/StatisitcsBoard/StatisticsBoardExt.swift
@@ -0,0 +1,22 @@
+//
+//  StatisticsBoardExt.swift
+//  Mentory
+//
+//  Created by 김민우 on 2/25/26.
+//
+import Foundation
+import Combine
+
+
+// MARK: object
+@MainActor
+final class StatisticsBoardExt: ObservableObject {
+    // MARK: core
+    
+    // MARK: state
+    
+    // MARK: action
+    
+    // MARK: core
+}
+
