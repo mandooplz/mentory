0bb43af1202acda8984bb36a8d2fd5a024a5e7d4
mandooplz <kmw4561@gmail.com>
2026-02-25 23:59:31 +0900

    MEN-19: StatisticsBoardExt 정리

---
+++ Mentory/MentoryTests/StatisticBoard/StatisticBoardTests.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticBoardTests.swift
+//  Mentory
+//
+//  Created by 김민우 on 2/25/26.
+//
+import Foundation
+import Testing
+@testable import Mentory
+
+
+// MARK: Tests
+@Suite("StatisticsBoard")
+struct StatisticsBoardTests {
+    struct InitRecords {
+        
+    }
+}
