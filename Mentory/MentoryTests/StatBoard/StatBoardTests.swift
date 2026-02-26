c75663fca1707976e1ab43d4a4710d0bf8eb2744
mandooplz <kmw4561@gmail.com>
2026-02-26 21:00:25 +0900

    MEN-20: StatBoard.loadRecords() 정의

--- Mentory/MentoryTests/StatBoard/StatBoardTests.swift
+++ Mentory/MentoryTests/StatBoard/StatBoardTests.swift
@@ -12,10 +12,56 @@ import Testing
 // MARK: Tests
 @Suite("StatBoard")
 struct StatBoardTests {
-    struct InitRecords {
+    struct Init {
+        let mentoryiOS: MentoryiOS
+        let statBoard: StatBoard
+        init() async throws {
+            self.mentoryiOS = await MentoryiOS()
+            self.statBoard = try await getStatBoardForTest(mentoryiOS)
+        }
         
+        @Test func isAllRecordsEmpty() async {
+            await #expect(statBoard.allRecords.isEmpty)
+        }
+    }
+    
+    struct LoadRecords {
+        let mentoryiOS: MentoryiOS
+        let statBoard: StatBoard
+        init() async throws {
+            self.mentoryiOS = await MentoryiOS()
+            self.statBoard = try await getStatBoardForTest(mentoryiOS)
+        }
+        
+        @Test func updateAllRecords() async throws {
+            // given
+            try await #require(statBoard.allRecords.isEmpty == true)
+            
+            // when
+            await statBoard.loadRecords()
+            
+            // then
+            await #expect(statBoard.allRecords.isEmpty == false)
+        }
     }
 }
 
 
 // MARK: Helpher
+private func getStatBoardForTest(_ mentoryiOS: MentoryiOS) async throws -> StatBoard {
+    // create Onboarding
+    await mentoryiOS.setUp()
+    
+    let onboarding = try #require(await mentoryiOS.onboarding)
+    
+    // create StatBoard
+    await onboarding.setName("테스트유저")
+    await onboarding.validateInput()
+    
+    await onboarding.next()
+    
+    // return StatBoard
+    let statBoard = try #require(await mentoryiOS.statBoard)
+    
+    return statBoard
+}
