fc534d81f26be37137b5390ff9f9ebdf771e0de5
mandooplz <kmw4561@gmail.com>
2026-03-08 23:51:16 +0900

    refactor: retire legacy mentory db adapters

--- MentoryDB/Sources/MentoryDBAdapter/DailyRecord/DailyRecordAdapter.swift
+++ MentoryDB/Sources/MentoryDBAdapter/DailyRecord/DailyRecordAdapter.swift
@@ -1,23 +1,23 @@
+////
+////  DailyRecordAdapter.swift
+////  Mentory
+////
+////  Created by 김민우 on 11/22/25.
+////
+//import Foundation
+//import MentoryDBCore
+//import Values
 //
-//  DailyRecordAdapter.swift
-//  Mentory
 //
-//  Created by 김민우 on 11/22/25.
+//// MARK: Adapter - wrong!
+//public nonisolated struct DailyRecordAdapter: DailyRecordInterface {
+//    private let dailyRecord: DailyRecord
 //
-import Foundation
-import MentoryDBCore
-import Values
-
-
-// MARK: Adapter - wrong!
-public nonisolated struct DailyRecordAdapter: DailyRecordInterface {
-    private let dailyRecord: DailyRecord
-
-    init(_ recordData: DailyRecord) {
-        self.dailyRecord = recordData
-    }
-    
-    public func getSuggestions() async throws -> [SuggestionData] {
-        await dailyRecord.getSuggestions()
-    }
-}
+//    init(_ recordData: DailyRecord) {
+//        self.dailyRecord = recordData
+//    }
+//
+//    public func getSuggestions() async throws -> [SuggestionData] {
+//        await dailyRecord.getSuggestions()
+//    }
+//}
